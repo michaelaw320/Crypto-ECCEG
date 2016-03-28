e012f1340faec20cb9832994df93fabd061fb880
michaelaw320 <michaelaw320@gmail.com>
2016-03-28 21:10:18 +0700

    Initial commit

---
+++ src/crypto/ecceg/utils/IOUtils.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crypto.ecceg.utils;
+
+/**
+ *
+ * @author Michael
+ * Only used in Input/Output tools (Read file, write file, etc.)
+ */
+public class IOUtils {
+    
+}
